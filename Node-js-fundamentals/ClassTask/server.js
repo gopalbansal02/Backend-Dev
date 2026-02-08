b78eb43a388934f6af357a890eccd30ed52597fe
Gopal Bansal <gopalbansal7078@hmail.com>
2026-02-08 19:23:24 +0530

    Added to class Practice

--- Node-js-fundamentals/ClassTask/server.js
+++ Node-js-fundamentals/ClassTask/server.js
@@ -1,8 +1,15 @@
+
+
+
+
+
+
 const http=require("http");
+const url=require("url");
 const server=http.createServer((req,res)=>{
-    res.writeHead(200,{"Content-Type":"text/html"});
-    res.end("Server is Running")
-})
+    const parsedURL=url.parse(req.url,true);
+    res.end("pathname: "+parsedURL.pathname);
+});
 server.listen(8000,()=>{
-    console.log("Server is Running on the port 8000")
+    console.log(`server is running on port http://localhost:8000`);
 })
---
+++ express-backend/express.js
@@ -0,0 +1,60 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json()); 
+
+const PORT = 8000;
+
+const students = [
+    { id: 1, name: "Alice", branch: "CSE" },
+    { id: 2, name: "Bob", branch: "ECE" },
+    { id: 3, name: "Charlie", branch: "MECH" },
+];
+
+app.get("/", (req, res) => {
+   res.send("Welcome to Expressjs Backend!");
+});
+
+app.get("/students", (req, res) => {
+    res.json(students);
+});
+
+app.get("/students/search", (req ,res) => {
+    const branch = req.query.branch;
+
+    if(!branch){
+        return res.json(students);
+    }
+
+    const foundStudents = students.filter(
+        s => s.branch === branch
+    );
+
+    res.json(foundStudents);
+});
+
+app.get("/students/:id", (req, res) => {
+    const id = req.params.id;
+
+    const arrayIndex = students.findIndex(
+        s => s.id == id
+    );
+
+    if(arrayIndex < 0){
+        return res.status(404).send("student not found");
+    }
+
+    res.json(students[arrayIndex]);
+});
+
+app.post("/students/register",(req,res)=>{
+    const data = req.body;
+
+    students.push(data);
+    res.status(201).json(data);
+
+});
+
+app.listen(PORT, () => {
+    console.log(Server is running on http://localhost:${PORT});
+)});
